74907b4b2bcb75b7ed90975672dd182a1b7f1cbd
Monkey <35855309+yiyuchenguang@users.noreply.github.com>
2022-06-28 14:16:28 +0800

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -8,6 +8,8 @@ class MySelenium(object):
     def __init__(self):
         self.basic_url = "https://www.baidu.com/"
         self.executable_path = r"./chromedriver_win32/chromedriver.exe"
+        self.headers = {'content-type': 'application/json',
+                        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
 
     @property
     def start_driver(self):
@@ -16,23 +18,28 @@ class MySelenium(object):
             self.browser.maximize_window()
         except SessionNotCreatedException:
             print("Chrome version unmatch. ")
-            return None
+
+            chrome = auto_download_chromedrive()
+            if chrome.start():
+                return 1
+            else:
+                return None
         return 1
 
     def request_url(self, url):
         """
         :param url:
         """
-        self.browser.get(url)
+        self.browser.get(url = url)
 
 if __name__ == '__main__':
-    start_time = time.time()
-    ms = MySelenium()
-    if not ms.start_driver:
-        chrome = auto_download_chromedrive()
-        chrome.start()
-        ms.start_driver
-        ms.request_url(ms.basic_url)
-        #ms.close()
-    test_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
-    print(test_time)
+
+        start_time = time.time()
+        ms = MySelenium()
+        print(ms)
+        print(ms.start_driver)
+        if ms.start_driver:
+                ms.request_url(ms.basic_url)
+            #ms.close()
+        test_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
+        print(test_time)
